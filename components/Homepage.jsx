c67a9df59bb8e91a5498adf2ab3f592eca5a44de
Thinushi <thinupavani@gmail.com>
2022-12-02 05:41:15 +0530

    update

---
+++ components/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <h1>Welcome</h1>
+    </div>
+  );
+};
+
+export default Homepage;
